d98980c4c1820422e90216259e35c5417096aec1
Brijesh Wawdhane <brijesh@wawdhane.com>
2020-10-09 21:49:24 +0530

    Auto detects current user as Author, no need to manually select

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,5 +1,6 @@
 class ArticlesController < ApplicationController
   before_action :set_article, only: [:show, :edit, :update, :destroy]
+  before_action :authenticate_user!, except [:index, :show]
 
   # GET /articles
   # GET /articles.json
@@ -25,7 +26,7 @@ class ArticlesController < ApplicationController
   # POST /articles.json
   def create
     @article = Article.new(article_params)
-
+    @article.user = current_user
     respond_to do |format|
       if @article.save
         format.html { redirect_to @article, notice: 'Article was successfully created.' }
@@ -69,6 +70,6 @@ class ArticlesController < ApplicationController
 
     # Only allow a list of trusted parameters through.
     def article_params
-      params.require(:article).permit(:title, :content, :category_id, :user_id)
+      params.require(:article).permit(:title, :content, :category_id)
     end
 end
